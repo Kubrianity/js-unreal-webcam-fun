01cde55a810ffc44aeef9b3d6e2b717d7f3e5862
Kubrianity <kubraaozerenn@gmail.com>
2022-07-18 12:54:50 +0300

    Add rgbSplit function

--- scripts.js
+++ scripts.js
@@ -15,8 +15,6 @@ function getVideo() {
         console.log("Unable to access media", err);
     })
 }
-getVideo();
-
 function paintToCanvas() {
     const {videoWidth: width, videoHeight: height} = video;
     canvas.width = width;
@@ -26,17 +24,17 @@ function paintToCanvas() {
         // take the pixels out
         let pixels = ctx.getImageData(0, 0, width, height);
         // change their rgb value
-        pixels = redEffect(pixels);
+        pixels = rgbSplit(pixels);
+        ctx.globalAlpha = 0.5; // transparency value
         // put them back
         ctx.putImageData(pixels, 0, 0);
-    }, 16)
+    }, 1)
 }
 function takePhoto() {
     // play the sound
     snap.currentTime = 0;
     snap.play();
-
-    // take the data out of the canvas
+     // take the data out of the canvas
     const data = canvas.toDataURL('image/jpeg');
     const link = document.createElement('a');
     link.href = data;
@@ -48,9 +46,19 @@ function redEffect(pixels) {
     for(let i = 0; i < pixels.data.length; i += 4) {
         pixels.data[i + 0] = pixels.data[i + 0] + 100; // red
         pixels.data[i + 1] = pixels.data[i + 1] - 50;  // green
-        pixels.data[i + 2] = pixels.data[i + 2] * 0.5; // blue
+        pixels.data[i + 2] = pixels.data[i + 2] - 0.5; // blue
     }
     return pixels;
 }
+function rgbSplit(pixels) {
+    for(let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]; // red
+        pixels.data[i + 200] = pixels.data[i + 1]; // green
+        pixels.data[i - 200] = pixels.data[i + 2]; // blue
+    }
+    return pixels;
+}
+getVideo();
 
 video.addEventListener("canplay", paintToCanvas);
+
